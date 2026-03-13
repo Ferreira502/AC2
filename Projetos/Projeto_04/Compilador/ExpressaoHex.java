4e90dd54bae654f27f61205f58f0ec5b5918ef11
Ferreira <gabrielferreirape502@gmail.com>
2026-03-13 20:02:06 -0300

    adding project04

---
+++ Projetos/Projeto_04/Compilador/ExpressaoHex.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+/**
+ * 
+ */
+public class ExpressaoHex
+{
+    public int a;
+    public int b;
+    public int op_code;
+
+    public void mostrarHex()
+    {
+        //System.out.print();
+    }
+}
